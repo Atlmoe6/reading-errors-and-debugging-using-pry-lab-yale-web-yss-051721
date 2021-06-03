d113409f44ef72b25bdb2a58cb322c2761105060
abstract-inheritance-c040 <doe.mahamud1@gmail.com>
2021-06-03 16:51:29 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -4,8 +4,6 @@ def start_game(player1, player2)
   "Hello #{player1} & #{player2}"
 end
 
-def play_game(player2)
- "#{player1} is better than #{player2}"
- binding.pry
- play_game
+def play_game(player1, player2)
+ p "#{player1} is better than #{player2}"
 end
